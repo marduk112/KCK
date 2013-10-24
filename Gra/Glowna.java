090e7410590cf4ba479dbf791c7653dd5ed2acdd
szymon12 <sw58132@st.amu.edu.pl>
2013-10-24 21:36:04 +0200

    Update Glowna.java

--- Gra/Glowna.java
+++ Gra/Glowna.java
@@ -2,8 +2,14 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package KCK.Gra;
+package Gra;
+
 import java.awt.EventQueue;
+
+import Gra.Plansze.Plansza_podst;
+
+
+
 
 /**
  *
@@ -11,16 +17,37 @@ import java.awt.EventQueue;
  */
 public class Glowna 
 {
-    public static void main(String[] args) 
+  
+
+	public static void main(String args[])
     {
-        EventQueue.invokeLater(new Runnable() 
-        {
-            @Override
-            public void run() 
-            {
-                        
-            }
-        });
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Szkielet frame = new Szkielet();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+      
+       //szkielet.getImg();
+       
+      // Plansza_podst plan = new Plansza_podst();
+      // plan.getIm();
+       
+       
+       
+       
+       
+     
+        
+        
         
     }
+    
+   
+    
+    
 }
