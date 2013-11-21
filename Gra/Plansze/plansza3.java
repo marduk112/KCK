74b74d7da56b1c80c5de117b2ec0f9be593cf93c
Szymon <szymon2wojcik@gmail.com>
2013-11-21 21:51:18 +0100

    Komendy+metody
    Ulepszona metoda walki, niewiele brakuje do pełnego działania

--- Gra/Plansze/plansza3.java
+++ Gra/Plansze/plansza3.java
@@ -24,31 +24,95 @@ public class plansza3 extends plansza_podst
         super("stage3.jpg");        
     }
     private String Utrata_Jednostek(float procent)
-    {
-        int pomoc;
+    {        
+        int pomoc=(int)(wojsko.Liczba_Jednostek()*procent);        
         if (wojsko.getObronah()<wojsko.getObronak())
         {
             if (wojsko.getObronah()<wojsko.getObronap())
             {
-                pomoc=(int)(wojsko.getHusarz()*procent);
-                wojsko.addHusarz(-pomoc);
-                if (pomoc>0.05*(wojsko.Liczba_Jednostek()+pomoc))
+                if (pomoc<=wojsko.getHusarz())
                 {
-                    if (wojsko.getObronap()<wojsko.getObronak())
+                    wojsko.addHusarz(-pomoc);                       
+                }      
+                else
+                {
+                    pomoc=pomoc-wojsko.getHusarz();
+                    wojsko.addHusarz(-wojsko.getHusarz());
+                    if (wojsko.getObronak()<wojsko.getObronap())
                     {
-                        pomoc=(int)((wojsko.getPiechur()*0.05)-pomoc);
-                        wojsko.addPiechur(pomoc);
+                        if (pomoc<=wojsko.getKusznik())
+                        {
+                            wojsko.addKusznik(-pomoc);
+                        }
+                        else
+                        {
+                            pomoc=pomoc-wojsko.getKusznik();
+                            wojsko.addKusznik(-wojsko.getKusznik());
+                            if (pomoc<=wojsko.getPiechur())
+                                wojsko.addPiechur(-pomoc);
+                            else
+                                wojsko.addPiechur(-wojsko.getPiechur());
+                        }
                     }
-                    //else
-                        
+                    else
+                    {
+                        if (pomoc<=wojsko.getPiechur())
+                        {
+                            wojsko.addPiechur(-pomoc);
+                        }
+                        else
+                        {
+                            pomoc=pomoc-wojsko.getPiechur();
+                            wojsko.addPiechur(-wojsko.getPiechur());
+                            if (pomoc<=wojsko.getKusznik())
+                                wojsko.addKusznik(-pomoc);
+                            else
+                                wojsko.addKusznik(-wojsko.getKusznik());
+                        }
+                    }
                 }
-                return "Utraciłeś "+pomoc+" husarzy";
             }
             else
             {
-                pomoc=(int)(wojsko.getPiechur()*procent);
-                wojsko.addPiechur(-pomoc);
-                return "Utraciłeś "+pomoc+" piechurów";
+                if (pomoc<=wojsko.getPiechur())                
+                    wojsko.addPiechur(-pomoc);                
+                else           
+                {
+                    pomoc=pomoc-wojsko.getPiechur();
+                    wojsko.addPiechur(-wojsko.getPiechur());
+                    if (wojsko.getObronak()<wojsko.getObronah())
+                    {
+                        if (pomoc<=wojsko.getKusznik())
+                        {
+                            wojsko.addKusznik(-pomoc);
+                        }
+                        else
+                        {
+                            pomoc=pomoc-wojsko.getKusznik();
+                            wojsko.addKusznik(-wojsko.getKusznik());
+                            if (pomoc<=wojsko.getHusarz())
+                            {
+                                wojsko.addHusarz(-pomoc);
+                            }
+                            else
+                                wojsko.addHusarz(wojsko.getHusarz());
+                        }
+                    }
+                    else
+                    {
+                        if (pomoc<=wojsko.getHusarz())
+                            wojsko.addHusarz(-pomoc);
+                        else
+                        {
+                            pomoc=pomoc-wojsko.getHusarz();
+                            wojsko.addHusarz(wojsko.getHusarz());
+                            if (pomoc<=wojsko.getKusznik())
+                                wojsko.addKusznik(-pomoc);
+                            else
+                                wojsko.addKusznik(-wojsko.getKusznik());
+                        }
+                    }
+                }                
             }
         }
         else
@@ -56,20 +120,23 @@ public class plansza3 extends plansza_podst
             if (wojsko.getObronak()<wojsko.getObronap())
             {
                 pomoc=(int)(wojsko.getKusznik()*procent);
-                wojsko.addKusznik(-pomoc);
-                return "Utraciłeś "+pomoc+" kuszników";
+                wojsko.addKusznik(-pomoc);                
             }
             else
             {
                 pomoc=(int)(wojsko.getPiechur()*procent);
-                wojsko.addPiechur(-pomoc);
-                return "Utraciłeś "+pomoc+" piechurów";
+                wojsko.addPiechur(-pomoc);               
             }
         }
+        return "Utraciłeś "+pomoc+" jednostek";
     }
     public String Walka()
     {
-        if (wojsko.getAttack()>wrog.getAttack() && wrog.getAttack()*1.3 >= wojsko.getAttack() && wojsko.getDefence()<wrog.getDefence() && wojsko.getDefence()>=wrog.getDefence()*0.8)
+        if (wojsko.getAttack()>wrog.getAttack() && (wrog.getAttack()*1.3 < wojsko.getAttack() && wojsko.getAttack() <= wrog.getAttack()*1.5) && wojsko.getDefence()<wrog.getDefence() && wojsko.getDefence()>=wrog.getDefence()*0.8)
+        {
+            return Utrata_Jednostek((float)0.5);
+        }
+        else if (wojsko.getAttack()>wrog.getAttack() && wrog.getAttack()*1.3 >= wojsko.getAttack() && wojsko.getDefence()<wrog.getDefence() && wojsko.getDefence()>=wrog.getDefence()*0.8)
         {
             return Utrata_Jednostek((float)0.7);
         }
